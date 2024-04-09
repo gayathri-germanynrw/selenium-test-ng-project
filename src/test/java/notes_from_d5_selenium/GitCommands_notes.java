6e822b7df0245575ff24bd5bd10351457c83b770
gayat <gayathrikasi@gmail.com>
2024-04-09 10:28:23 +0200

    Git Commands notes file are added

---
+++ src/test/java/notes_from_d5_selenium/GitCommands_notes.java
@@ -0,0 +1,15 @@
+package notes_from_d5_selenium;
+
+public class GitCommands_notes {
+    /*
+    enable VCS ---> git init
+add files into staging area  ---> git add .
+put the changes into local repo --> git commit -m "message"
+add remote repo to intellij ---> git remote add origin https://github.com/<username>/<reponame>.git
+push the codes ---> git push origin master
+create a branch ---> git branch <branch name>
+change branch ---> git checkout <branch name>
+push specific branch --->  git push origin <branch name>
+clone the project ---> git clone https://github.com/<username>/<reponame>.git
+     */
+}
